c53d942129df1dbcf72f0b1b3ef223b34f6a2382
Andrew Zimolzak <andyzimolzak@gmail.com>
2013-11-13 10:04:42 -0500

    Successfully merged the 2 functions.

--- vcover.py
+++ vcover.py
@@ -3,33 +3,22 @@ def vertex_cover_tree(input_graph):
     assignment = [None]*n
     return recursive_vertex_cover(input_graph, assignment)
 
-def validity_check(graph, cover):
-    covered = 0
-    edges = 0
-    for i in range(0, len(graph)):
-        for j in range(i+1, len(graph[i])):
-            edges = edges + graph[i][j]
-            covered = covered + graph[i][j] * ((cover[i]==1) or (cover[j]==1)) #NB x==1. Be prepared for None values.
-    if None in cover:
-        return None
-    elif covered < edges:
-        return False
-    elif covered == edges:
-        return True
-    else:
-        raise
-
 def recursive_vertex_cover(input_graph, assignment):
     # - Check if it's still possible to construct a valid vertex cover,
     # if not, return float("inf") (the Python expression for infinity)
     # MORE COMPLEX CRUD
-    validity = validity_check(input_graph, assignment)
-    if validity == True:
-        return sum(assignment)
-    elif validity == False:
-        return float("inf")
-    elif validity == None:
+    covered = 0
+    edges = 0
+    for i in range(0, len(input_graph)):
+        for j in range(i+1, len(input_graph[i])):
+            edges = edges + input_graph[i][j]
+            covered = covered + input_graph[i][j] * ((assignment[i]==1) or (assignment[j]==1)) #NB x==1.
+    if None in assignment:
         v = assignment.index(None)
+    elif covered < edges:
+        return float("inf")
+    elif covered == edges:
+        return sum(assignment)
     else:
         raise
     # END OF YOUR CODE. The following code takes care of the recursive
