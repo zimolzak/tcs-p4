70563e0932b73d8f9cef2f4b1bb7c86afacf5105
Andrew Zimolzak <andyzimolzak@gmail.com>
2013-11-14 18:13:11 -0500

    Much revised. Make one sub per rule. Need to improve printing.

--- satpp.py
+++ satpp.py
@@ -9,7 +9,7 @@
 # 4) Remove all variables that evaluate to 'FALSE' (i.e., all variables x
 # that are set to FALSE and all variables not(x) where x is set to TRUE).
 # If this results in an empty clause, then the input formula has no satisfying
-# assignment and the function should return the Boolean formula [[1,-1]]
+# assignment and the function should return the Boolean formula [[1],[-1]]
 #
 # The challenging part is implementing Rule 2, for your function must
 # perform the data reductions exhaustively, that is, until they can no
@@ -21,62 +21,132 @@
 #
 # If through the pre-processing steps you are able to determine that a
 # SAT problem is satisfiable then return []. Likewise, if you
-# determine that it is unsatisfiable then return [[1,-1]]. Otherwise,
+# determine that it is unsatisfiable then return [[1],[-1]]. Otherwise,
 # return the remaining clauses.
 
-def sat_preprocessing(num_variables, clauses):
-    assignment = [None] * num_variables
+def rule1(assignment, clauses):
     for row in clauses:
-        if len(row)==1:
-            return [1,-1]
-            #assignment[abs(row[0])] = (row[0] < 0)
-    print assignment
-    return [1,-1]
+        if len(row)==1: #rule1
+            assignment[abs(row[0])] = (row[0] > 0)
+    return assignment
 
-def test():
-    assert [] == sat_preprocessing(1, [[1]])
+def rule2(assignment, clauses):
+    occurrences = [0] * (len(assignment)) # 0 means 0. 1 or -1 means 1. 2 means 2+.
+    for row in clauses:# count up occurrences in prep for rule 2
+        for term in row: 
+            if occurrences[abs(term)] > 1:
+                continue
+            elif abs(occurrences[abs(term)]) == 1:
+                occurrences[abs(term)] = 2 # occurs > 1 time
+            elif occurrences[abs(term)] == 0 and assignment[abs(term)]==None:
+                occurrences[abs(term)] = abs(term)/term # 1 or -1 if exactly 1 time
+    for var_num in range(1,len(occurrences)): # modify assignment for any vars occurring 1 X.
+        if abs(occurrences[var_num])==1 and assignment[var_num]==None:
+            assignment[var_num] = (occurrences[var_num] > 0)
+    return assignment
 
-    assert [[1,-1]] == sat_preprocessing(1, [[1], [-1]])
+def rule3(assignment, clauses):
+    for i in range(len(clauses)):
+        for j in range(len(clauses[i])):
+            if clauses[i] == "sat":
+                continue
+            if assignment[abs(clauses[i][j])]==True:
+                clauses[i] = "sat" # don't delete right away or it screws up index counting
+    while "sat" in clauses:
+        clauses.remove("sat")
+    return clauses
 
-    assert [] == sat_preprocessing(4,[[4], 
-                                      [-3, -1], 
-                                      [3, -4, 2, 1], 
-                                      [1, -3, 4],
-                                      [-1, -3, -4, 2], 
-                                      [4, 3, 1, 2], 
-                                      [4, 3],
-                                      [1, 3, -4], 
-                                      [3, -4, 1], 
-                                      [-1]])
+def rule4(assignment, clauses):
+    for var_num in range(1,len(assignment)):
+        if assignment[var_num] == None:
+            continue
+        elif assignment[var_num] == False:
+            val_to_remove = -1 * var_num
+        elif assignment[var_num] == True:
+            val_to_remove = var_num
+        for row in clauses:
+            while val_to_remove in row:
+                row.remove(val_to_remove)
+            if row == []:
+                return [[1],[-1]]
+    return clauses
 
-    assert [[1,-1]] == sat_preprocessing(5,[[4, -2], 
-                                            [-1, -2], 
-                                            [1], 
-                                            [-4],
-                                            [5, 1, 4, -2, 3], 
-                                            [-1, 2, 3, 5],
-                                            [-3, -1], 
-                                            [-4], 
-                                            [4, -1, 2]])
-    
-    ans = [[5, 6, 2, 4], 
-           [3, 5, 2, 4], 
-           [-5, 2, 3], 
-           [-3, 2, -5, 6, -4]]
-    
-    assert ans == sat_preprocessing(6, [[-5, 3, 2, 6, 1], 
-                                        [5, 6, 2, 4],
-                                        [3, 5, 2, -1, 4], 
-                                        [1], 
-                                        [2, 1, 4, 3, 6],
-                                        [-1, -5, 2, 3], 
-                                        [-3, 2, -5, 6, -4]])
+def sat_preprocessing(num_variables, clauses):
+    assignment = [None] * (num_variables + 1) # assignment[0] is dummy
+    exhausted = 0
 
-# test()
+    assignment=rule1(assignment, clauses)
+
+    assignment=rule2(assignment, clauses)
+
+    clauses=rule3(assignment, clauses)
+
+    clauses=rule4(assignment, clauses)
+
+    # oldass=ass
+    # ass=rule(ass,clau)
+    # if oldass[1:len(oldass)] == ass[1:len(ass)]:
+    #     then we are unchanged
+            
+    return clauses
 
 s1 = [[1]]
-
 s2 = [[1], [-1]]
+
+s_sing = [[-2], 
+          [-1, -2], 
+          [1], 
+          [5, 1, -2, 3], 
+          [-1, 2, 3, 5],
+          [-3, -1], 
+          [4, -1, 2]] #x4 apears just once
+
+print "rule2 test"
+
+sat_preprocessing(5, s_sing)
+
+
+
+# def test():
+#     assert [] == sat_preprocessing(1, [[1]])
+
+#     assert [[1],[-1]] == sat_preprocessing(1, [[1], [-1]])
+
+#     assert [] == sat_preprocessing(4,[[4], 
+#                                       [-3, -1], 
+#                                       [3, -4, 2, 1], 
+#                                       [1, -3, 4],
+#                                       [-1, -3, -4, 2], 
+#                                       [4, 3, 1, 2], 
+#                                       [4, 3],
+#                                       [1, 3, -4], 
+#                                       [3, -4, 1], 
+#                                       [-1]])
+
+#     assert [[1],[-1]] == sat_preprocessing(5,[[4, -2], 
+#                                             [-1, -2], 
+#                                             [1], 
+#                                             [-4],
+#                                             [5, 1, 4, -2, 3], 
+#                                             [-1, 2, 3, 5],
+#                                             [-3, -1], 
+#                                             [-4], 
+#                                             [4, -1, 2]])
+    
+#     ans = [[5, 6, 2, 4], 
+#            [3, 5, 2, 4], 
+#            [-5, 2, 3], 
+#            [-3, 2, -5, 6, -4]]
+    
+#     assert ans == sat_preprocessing(6, [[-5, 3, 2, 6, 1], 
+#                                         [5, 6, 2, 4],
+#                                         [3, 5, 2, -1, 4], 
+#                                         [1], 
+#                                         [2, 1, 4, 3, 6],
+#                                         [-1, -5, 2, 3], 
+#                                         [-3, 2, -5, 6, -4]])
+
+# test()
 
 s4 = [[4], 
       [-3, -1], 
@@ -113,7 +183,7 @@ s6 = [[-5, 3, 2, 6, 1],
       [-3, 2, -5, 6, -4]]
 
 print sat_preprocessing(1, s1), "expect []"
-print sat_preprocessing(1, s2), "expect [[1,-1]]"
+print sat_preprocessing(1, s2), "expect [[1],[-1]]"
 print sat_preprocessing(4, s4), "expect []"
-print sat_preprocessing(5, s5), "expect [[1,-1]]"
+print sat_preprocessing(5, s5), "expect [[1],[-1]]"
 print sat_preprocessing(6, s6), "expect", ans
